7fc0d678c5204176da0f7839272f410700780c3e
Deeptiwani <Deeptiwani@host.docker.internal>
2024-08-29 18:32:43 +0530

    Initial Commit.

---
+++ SpringBootAdmin/src/test/java/com/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springbootadmin;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
